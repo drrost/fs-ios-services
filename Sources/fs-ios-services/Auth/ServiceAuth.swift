0a4ae22dc5e7f64d7951b977a283b1bf3e6e3ce7
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-11-09 22:08:42 +0200

    Improve auth service

--- Sources/fs-ios-services/Auth/ServiceAuth.swift
+++ Sources/fs-ios-services/Auth/ServiceAuth.swift
@@ -7,14 +7,25 @@
 
 import Foundation
 
+public typealias LoginCompletion = ((String?, Error?) -> Void)
+
 public protocol IServiceAuth: IService {
 
-    func login(_ login: String, _ password: String, _ completion: (Bool) -> Void)
+    func login(_ login: String, _ password: String, _ completion: LoginCompletion)
 }
 
 class ServiceAuth: IServiceAuth {
 
-    func login(_ login: String, _ password: String, _ completion: (Bool) -> Void) {
-        completion(true)
+    enum AuthError: Error {
+        case invalidLoginOrPassword
+    }
+
+    func login(_ login: String, _ password: String, _ completion: LoginCompletion) {
+
+        if login == "user" && password == "password" {
+            completion(UUID().uuidString, nil)
+        } else {
+            completion(nil, AuthError.invalidLoginOrPassword)
+        }
     }
 }
